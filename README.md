8f859ab58f450c71efdd7ece3aec3e528a07309b
Traian Svinti <traian.svinti@gmail.com>
2020-02-13 18:20:02 +0000

    Script and README added/updated

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # pipenv-ify_pip-tools
-Script to run pip-tools similar to pipenv's environment and syncing automation
+Script to run pip-tools similar to pipenv's environment and syncing automation.
+This uses virtualenv as the env tool.
+
+___
+
+If you are still using Pipenv and you want to migrate, follow this guide by Nick Timkovich (@nicktimko):
+- https://medium.com/telnyx-engineering/rip-pipenv-tried-too-hard-do-what-you-need-with-pip-tools-d500edc161d4
+___
+
+### Usage
+1. Clone repo
+2. `cd pipenv-ify_pip-tools`
+3. `mv pipenv-ify_pip-tools.sh /usr/local/bin` - This makes the file available in $PATH
+4. `sudo chmod +x /usr/local/bin/pipenv-ify_pip-tools.sh`
+
+___
+
+The intended is to set an override to `cd` so that it runs when 
+going into a project folder with a `requirements.in` file:
+
+`~/.zshrc or ~/.bashrc`
+```
+function cd {
+    builtin cd "$@"
+    source pipenv-ify_pip-tools.sh
+    ls
+}
+```
+
+___
---
+++ pipenv-ify_pip-tools.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Env vars
+ENV_NAME="${PWD##*/}"
+ENV_DIR="$HOME/.venvs/${ENV_NAME}"
+INSTALLED=$(pip list | grep virtualenv)
+
+# Emojis
+THUMB_DOWN="\xF0\x9F\x91\x8E"
+FIRE="\xF0\x9F\x94\xA5"
+HUNDRED="\xF0\x9F\x92\xAF"
+OK="\xF0\x9F\x91\x8C"
+MONKEY="\xF0\x9F\x99\x8A"
+
+function compile_and_sync {
+    # Check if requirements.txt exists
+    if [ ! -f requirements.txt ]; then
+        printf " ${THUMB_DOWN} --> No \"requirements.txt\" found - Compiling hashes to \"requirements.txt\" "
+
+        # Compile requirements.in to requirements.txt with hashes
+        pip-compile --quiet \
+            --generate-hashes \
+            --output-file=requirements.txt \
+            requirements.in
+    fi
+
+    printf "\n ${FIRE} --> Syncing requirements.txt with virtual environment \n\n"
+
+    # Sync requiremnts.txt hashes with venv
+    pip-sync -q
+}
+
+function check_env {
+    # Check to see if already activated to avoid redundant activating
+    if [ "$VIRTUAL_ENV" != "${ENV_DIR}" ]; then
+
+        printf "\n ${HUNDRED} --> Activating virtualenv \"$ENV_NAME\" "
+
+        # Activate the virtual environment
+        source ${ENV_DIR}/bin/activate
+    fi
+}
+
+function create_env {
+    printf "\n ${OK} --> Creating virtualenv named ${ENV_NAME} in ${ENV_DIR} "
+
+    # Create virtual env in ~/.venvs with name of project
+    virtualenv -q ${ENV_DIR}
+
+    # Activate the virtual environment
+    source ${ENV_DIR}/bin/activate
+
+    # Install pip-tools in environment
+    pip3 install -q pip-tools
+}
+
+function run {
+    # Check if requirements.in exists
+    if [ -f requirements.in ]; then
+
+        # Check if env dir already exists
+        if [ -e "${ENV_DIR}" ]; then
+            check_env
+        else
+            create_env
+        fi
+
+        compile_and_sync
+    fi
+}
+
+# Check if virtualenv is installed
+if [ -n "$INSTALLED" ]; then
+    run
+else
+    # If not already in virtual environment, install virtualenv and run
+    if [ "$VIRTUAL_ENV" != "${ENV_DIR}" ]; then
+        printf "\n ${MONKEY} --> Virtualenv not installed - Installing now "
+        pip3 install -q virtualenv
+        run
+    fi
+fi
